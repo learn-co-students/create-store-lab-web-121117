e841a0599d3efa0563eddc5fc96e89af160d41d5
github username <github email address>
2018-03-11 20:16:18 -0400

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class UserInput extends Component {
+  state = {
+    hometown:'',
+    username:''
+  }
+
+  handleChange = (e) =>{
+    this.setState({
+      [e.target.name]: e.target.value
+    },()=>{console.log(this.state)})
+  }
+
+  handleSubmit = (e) =>{
+    e.preventDefault()
+    this.props.store.dispatch({
+      type: "ADD_USER",
+      user: this.state
+    })
+
+  }
+
   render() {
     return(
       <div>
-        user input
+        <form onSubmit={this.handleSubmit}>
+          <input name='username' type='text' onChange={this.handleChange} ></input>
+          <input name='hometown' type='text' onChange={this.handleChange} ></input>
+          <input type='submit'></input>
+        </form>
       </div>
     );
   }
--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,16 @@
-export default function manageUsers(state, action){
+let defaultState={
+  users:[]
+}
+
+export default function manageUsers(state=defaultState, action){
+  switch (action.type){
+    case 'ADD_USER':
+      console.log(action.user)
+      return{
+        users: [...state.users, action.user]
+      }
+
+    default:
+      return state
+  }
 }
